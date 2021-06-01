f81d54bab2adc1434d1267055b9c9da184719132
GonzaVice <givicente@miuandes.cl>
2021-06-01 17:13:29 -0400

    feat: Cambio de arreglo a vector en clase Carta

--- main.cpp
+++ main.cpp
@@ -9,18 +9,18 @@
 
 using namespace std;
 
-Carta * deck_setup()
+vector<Carta> deck_setup()
 {
-    Carta * baraja = new Carta[52];
+    vector<Carta> baraja = vector<Carta>();
 
     for(int i = 0; i < 13; i++)
     {
         for(int j = 0; j < 4; j++)
         {
-            if(j == 0) baraja[i*4 + j] = Carta(i+1, 'C');
-            else if(j == 1) baraja[i*4 + j] = Carta(i+1, 'D');
-            else if(j == 2) baraja[i*4 + j] = Carta(i+1, 'P');
-            else baraja[i*4 + j] = Carta(i+1, 'T');
+            if(j == 0) baraja.push_back(Carta(i+1, 'C'));
+            else if(j == 1) baraja.push_back(Carta(i+1, 'D'));
+            else if(j == 2) baraja.push_back(Carta(i+1, 'P'));
+            else baraja.push_back(Carta(i+1, 'T'));
         }
     }
     return baraja;
@@ -34,13 +34,13 @@ int main()
     Jugador player1 = Jugador("Clark", 1000);
     //player1.mostrar_info();
 
-    Carta * baraja = deck_setup();
+    vector<Carta> baraja = deck_setup();
+
+
     for(int i = 0; i < 52; i++)
     {
         baraja[i].mostrar_info();
     }
-
-    delete baraja;
 
     return 0;
 }
